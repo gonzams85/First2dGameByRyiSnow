8b54999425e568f5e9e37a559e4655cad0f75e55
gonzams85 <gonzams85@gmail.com>
2023-01-12 17:07:54 -0300

    Improved draw time from 650000ms to around 75000ms 150000ms. code looks a little bit cleaner.

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -1,12 +1,14 @@
 package main;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
 
     public boolean upPressed,downPressed,leftPressed,rightPressed;
-
+    //DEBUG
+    boolean checkDrawTime = false;
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -31,7 +33,15 @@ public class KeyHandler implements KeyListener {
             rightPressed=true;
         }
 
-
+        //DEBUG
+        if(code == KeyEvent.VK_T){
+            if(checkDrawTime == false) {
+                checkDrawTime = true;
+            }
+            else if(checkDrawTime == true) {
+                checkDrawTime = false;
+            }
+        }
     }
 
     @Override
